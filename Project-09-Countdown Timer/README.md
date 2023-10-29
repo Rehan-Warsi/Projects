147917bf1a119d9f9e646a543a55d62fc4ade03f
Rehan <142205041+Rehan-Warsi@users.noreply.github.com>
2023-10-29 20:18:11 +0500

    Add files via upload

---
+++ Project-09-Countdown Timer/README.md
@@ -0,0 +1,23 @@
+# INTERACTIVE COUNTDOWN TIMER 
+
+CLI based Interactive Countdown Timer By Rehan Warsi
+
+---
+
+# Description 
+
+CLI based Interactive Countdown Timer
+
+
+# Installation
+
+    npm i @rehan-warsi/countdown-timer-g
+
+---
+
+# Command
+
+    npx main
+
+---
+
---
+++ Project-09-Countdown Timer/main.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+import { differenceInSeconds } from "date-fns";
+console.clear();
+console.log(chalk.bold.rgb(204, 204, 204)(`\n   <<<============================================>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`<<<=======>>>  ${chalk.redBright.bold('CLI BASED COUNTDOWN TIMER')}  <<<========>>>`));
+console.log(chalk.bold.rgb(204, 204, 204)(`   <<<============================================>>>\n`));
+let name = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Please Enter the amount of second",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "Please Enter valid Number";
+        }
+        else {
+            if (input > 60) {
+                return "Seconds must be in 60";
+            }
+            else {
+                return true;
+            }
+        }
+    }
+});
+function startTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval((() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log("Timer has Expired");
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }), 1000);
+}
+startTime(name.userInput);
